12840af478d0bc2ff02156b750ab67856c967565
unknown <NE6U@VDIDev-28.manserv.net>
2015-05-21 13:30:25 +0200

    Restart timer with duration parameter.

--- index.js
+++ index.js
@@ -3,43 +3,44 @@
 var notifier = require('node-notifier');
 var util = require('util');
 
+var duration = parseInt(process.argv[process.argv.length - 1], 10);
+
 function clearConsole() {
-    util.print("\u001b[2J\u001b[0;0H");
+	util.print("\u001b[2J\u001b[0;0H");
 }
 
 function stopPomodoro() {
-    notifier.notify({
-        title:'pomodoro finished',
-        message:'pomodoro finished, click to start new pomodoro!',
-        wait: 'true'
-    })
+	notifier.notify({
+		title: 'pomodoro finished',
+		message: 'pomodoro finished, click to start new pomodoro!',
+		wait: 'true'
+	})
 }
 
 function startTimer(durationInMinutes) {
-    var totalSeconds = durationInMinutes * 60;
+	var totalSeconds = durationInMinutes * 60;
 
-    var intervalId = setInterval(function () {
-        minutes = parseInt(totalSeconds / 60, 10);
-        seconds = parseInt(totalSeconds % 60, 10);
+	var intervalId = setInterval(function() {
+		minutes = parseInt(totalSeconds / 60, 10);
+		seconds = parseInt(totalSeconds % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        clearConsole();
-        console.log(minutes + ":" + seconds);
+		clearConsole();
+		console.log(minutes + ":" + seconds);
 
-        if (--totalSeconds < 0) {
-            stopPomodoro();
-            clearInterval(intervalId);
-        }
-    }, 1000);
+		if (--totalSeconds < 0) {
+			stopPomodoro();
+			clearInterval(intervalId);
+		}
+	}, 1000);
 }
 
 
-notifier.on('click', function (notifierObject, options) {
-  startTimer(1);
+notifier.on('click', function(notifierObject, options) {
+	startTimer(duration);
 });
 
-var duration = parseInt(process.argv[process.argv.length -1], 10);
 
 startTimer(duration);
